765f86e121dd34435435e620daa1484d033f8bf9
halcyoona <halcyoona@gmail.com>
2020-12-01 15:10:36 +0500

    simple

---
+++ airstriker/airstriker_simple.py
@@ -0,0 +1,16 @@
+import retro
+
+def main():
+    env = retro.make(game='Airstriker-Genesis')
+    obs = env.reset()
+    while True:
+        obs, rew, done, info = env.step(env.action_space.sample())
+        env.render()
+        if done:
+            obs = env.reset()
+    env.close()
+
+
+if __name__ == "__main__":
+    retro.data.list_games()
+    main()
